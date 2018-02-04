6690a6466514cbf573a1204d8478af4743ba91fa
Jake Bladt <jake@jakebladt.com>
2018-02-03 20:28:45 -0500

    +domain

---
+++ StockArt.Domain/StockArt.Domain/ImageSetSubject.cs
@@ -0,0 +1,10 @@
+﻿namespace StockArt.Domain
+{
+    public class ImageSetSubject
+    {
+        public string ImageSetName { get; set; }
+        public ImageSet Set { get; set; }
+        public int SubjectID { get; set; }
+        public Subject Subject { get; set; }
+    }
+}
