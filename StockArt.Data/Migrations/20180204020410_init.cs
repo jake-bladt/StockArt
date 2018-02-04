7aba6d167f8e0fbb74708ebdd5dba65d06529f83
Jake Bladt <jake@jakebladt.com>
2018-02-03 21:05:21 -0500

    +initial migration

---
+++ StockArt.Data/Migrations/20180204020410_init.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace StockArt.Data.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Subjects",
+                columns: table => new
+                {
+                    SubjectID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    DisplayName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subjects", x => x.SubjectID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Subjects");
+        }
+    }
+}
--- StockArt.Data/StockArtDBContext.cs
+++ StockArt.Data/StockArtDBContext.cs
@@ -1,10 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.EntityFrameworkCore;
+
+using StockArt.Domain;
+
 
 namespace StockArt.Data
 {
-    class StockArtDBContext
+    public class StockArtDBContext : DbContext
     {
+        public DbSet<Subject> Subjects { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=sart00;Integrated Security=True;Pooling=False");
+        }
+
     }
 }
