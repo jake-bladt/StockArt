08598ee0d073ad17dae23b0a78940cdb3afa5ad0
Jake Bladt <jake@jakebladt.com>
2018-02-04 11:17:23 -0500

    name conversion

--- StockArt.Migrations/FromCatalogDirectory.cs
+++ StockArt.Migrations/FromCatalogDirectory.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 
 using StockArt.Data;
 using StockArt.Domain;
+using System.Text;
 
 namespace StockArt.Migrations
 {
@@ -39,7 +40,16 @@ namespace StockArt.Migrations
 
             var ret = new MigrationResult();
 
+            var rootDI = new DirectoryInfo(_settings.Root);
+            var subjectDIs = rootDI.GetDirectories();
+            Announce(String.Format("{0} subject directories found.", subjectDIs.Length));
 
+            foreach(var sdi in subjectDIs)
+            {
+                var name = sdi.Name;
+                var imgCount = sdi.GetFiles().Length;
+
+            }
 
             return ret;
 
@@ -48,6 +58,19 @@ namespace StockArt.Migrations
         protected void Announce(string msg)
         {
             if (null != _messageHook) _messageHook(msg);
+        }
+
+        protected string SetNameToSubjectName(string setName)
+        {
+            var letters = setName.ToCharArray();
+            var r = letters[0].ToString();
+            var builder = new StringBuilder(prev.ToUpper());
+            for(int i = 1; i < letters.Length; i++)
+            {
+            
+            }
+
+            return builder.ToString();
         }
         
     }
