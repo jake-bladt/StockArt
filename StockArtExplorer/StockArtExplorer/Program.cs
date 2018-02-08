5c47aa44b35d0a8fca034b2e1a68bdc9d727fbb7
Jake Bladt <jake@jakebladt.com>
2018-02-07 20:25:42 -0500

    boilerplate site

---
+++ StockArtExplorer/StockArtExplorer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace StockArtExplorer
+{
+    public class Program
+    {
+        public static IConfigurationRoot Configuration { get; set; }
+
+        public static void Main(string[] args)
+        {
+            var configBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+            Configuration = configBuilder.Build();
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseWebRoot("wwwroot")
+                .UseIISIntegration()
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.AddConsole();
+                    logging.AddDebug();
+                })
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+
+    }
+}
---
+++ StockArtExplorer/StockArtExplorer/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+using Newtonsoft.Json.Serialization;
+
+namespace StockArtExplorer
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+             .AddMvc()
+             .AddJsonOptions(options => options.SerializerSettings.ContractResolver = new DefaultContractResolver());
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
